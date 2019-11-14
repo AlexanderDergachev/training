bd9af00eea1db2999efc3ae1b623ab40924f0153
AlexanderDergachev <31667079+AlexanderDergachev@users.noreply.github.com>
2019-11-15 01:14:38 +0300

    Update App.js

--- projects/my-dnd/src/App.js
+++ projects/my-dnd/src/App.js
@@ -5,24 +5,61 @@ import initialData from './initial-data'
 import Column from './Column'
 import { DragDropContext } from 'react-beautiful-dnd'
 export default class App extends Component {
-  state = initialData;
+	state = initialData;
 
-  onDragEnd = result => {
-    //tii
-  }
+	onDragEnd = result => {
+		const { destination, source, draggableId } = result;
+		// console.log("destination.droppableId:" + destination.droppableId);
+		// console.log("source.droppableId:" + source.droppableId);
+		// console.log("draggableId:" + draggableId);
 
-  render() {
-    return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        {
-          this.state.columnOrder.map(columnId => {
-            const column = this.state.columns[columnId];
-            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-            return <Column key={column.id} column={column} tasks={tasks} />
-          })
-        }
-      </DragDropContext>
-    )
-  }
+		if (!destination) {
+			return;
+		}
+
+		if (destination.droppableId === source.droppableId &&
+			destination.index === source.index
+		) {
+			return;
+		}
+
+		const column = this.state.columns[source.droppableId];
+		
+		
+		const newTaskIds = Array.from(column.taskIds);
+
+		newTaskIds.splice(source.index, 1);
+		
+		newTaskIds.splice(destination.index, 0, draggableId);
+
+		const newColumn = {
+			...column,
+			taskIds: newTaskIds,
+		};
+
+		const newState = {
+			...this.state,
+			columns: {
+				...this.state.columns,
+				[newColumn.id]: newColumn,
+			},
+		};
+
+		this.setState(newState);
+	};
+
+	render() {
+		return (
+			<DragDropContext onDragEnd={this.onDragEnd}>
+				{
+					this.state.columnOrder.map(columnId => {
+						const column = this.state.columns[columnId];
+						const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+						return <Column key={column.id} column={column} tasks={tasks} />
+					})
+				}
+			</DragDropContext>
+		)
+	}
 }
 
