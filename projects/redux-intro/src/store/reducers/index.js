67ac2be6984936245c5875460ed9c413fc61f3c6
AlexanderDergachev <31667079+AlexanderDergachev@users.noreply.github.com>
2020-01-25 00:45:08 +0300

    clean up

--- projects/redux-intro/src/store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import boards from './boards';
-import notes from './notes';
-
-const rootReducer = combineReducers({ boards, notes });
-
-export default rootReducer;
